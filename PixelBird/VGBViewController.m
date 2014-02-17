edfc4dca9f0a70c4d12b379d144ac307a6b45441
tearsofphoenix <tearsofphoenix@icloud.com>
2014-02-17 16:22:58 +0800

    working on.

--- PixelBird/VGBViewController.m
+++ PixelBird/VGBViewController.m
@@ -8,22 +8,77 @@
 
 #import "VGBViewController.h"
 
-@interface VGBViewController ()
+@interface VGBViewController ()<UIWebViewDelegate, UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) UIWebView *webView;
 
 @end
 
 @implementation VGBViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect bounds = [[self view] bounds];
+    _webView = [[UIWebView alloc] initWithFrame: bounds];
+    [[self view] addSubview: _webView];
+    
+    [_webView setDelegate: self];
+    
+    NSURL *fileURL = [[NSBundle mainBundle] URLForResource: @"game"
+                                             withExtension: @"html"
+                                              subdirectory: @"bird"];
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL: fileURL];
+    [_webView loadRequest: request];
+//    
+////    UIView *view = [[UIView alloc] initWithFrame: bounds];
+////    [[self view] addSubview: view];
+//    
+//    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget: self
+//                                                                                 action: @selector(_handleTappedEvent:)];
+////    [tapGesture setNumberOfTapsRequired: 2];
+//    [tapGesture setDelegate: self];
+//    
+//    [_webView addGestureRecognizer: tapGesture];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)touchesBegan: (NSSet *)touches
+           withEvent: (UIEvent *)event
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (BOOL)           webView: (UIWebView *)webView
+shouldStartLoadWithRequest: (NSURLRequest *)request
+            navigationType: (UIWebViewNavigationType)navigationType
+{
+    NSLog(@"%@", [request URL]);
+    return YES;
+}
+
+- (void)     webView: (UIWebView *)webView
+didFailLoadWithError: (NSError *)error
+{
+    NSLog(@"%@", error);
+}
+
+- (void)_handleTappedEvent: (id)sender
+{
+    [_webView stringByEvaluatingJavaScriptFromString: @"handleJumpStart();"];
+}
+
+- (BOOL)                         gestureRecognizer: (UIGestureRecognizer *)gestureRecognizer
+shouldRecognizeSimultaneouslyWithGestureRecognizer: (UIGestureRecognizer *)otherGestureRecognizer
+{
+    return YES;
 }
 
 @end
